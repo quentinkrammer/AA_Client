11273a8d31dc75a2aafc7773ace942e4727e0fe9
Bastian Opatz (Student) <ip15oheg@LHFT.DE>
2019-10-30 13:47:55 +0100

    created class prototype of serialCommunication

--- src/serialCommunication.py
+++ src/serialCommunication.py
@@ -4,39 +4,55 @@ Created on Thu Oct 24 13:12:44 2019
 @author: ip15oheg
 """
 import serial
+
+class serialCommunication:    
+    
+    def __init__(self, baudrate=115200, port="COM16"):
+        self.ser = serial.Serial()
+        self.baudrate = baudrate
+        self.port = port
+        #ser.timeout = 0.500
+        self.ser.open()
  
-def readNextMsg():
-    msg = ""
-    endOfMsg = False
-    while(not endOfMsg):
-        char = ser.read().decode()        
-        if char == "\n":
-            endOfMsg = True
-        else:
-            msg += char  
-    return msg
-
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.port = 'COM16'
-ser.timeout = 0.500
-ser.open()
-
-try:
-    cmd = "SYS:OFF"
-    cmd += "\n"
-    ser.write(cmd.encode())
-    echo = readNextMsg()
-    print(echo)
-    reply = readNextMsg()
-    print(reply)
-    #print(readNextMsg())
-    #print(readNextMsg())
-
-except Exception as e:
-    print(e)
+    def readNextMsg(self):
+        msg = ""
+        endOfMsg = False
+        while(not endOfMsg):
+            char = self.ser.read().decode()        
+            if char == "\n":
+                endOfMsg = True
+            else:
+                msg += char  
+        return msg
     
-finally:   
-    ser.close()
+    def parseCmd(self, cmd):
+        replies = []
+        try:            
+            cmd += "\n"
+            self.ser.write(cmd.encode())
+            if "ANT:ACT" in cmd:
+                replies.append (self.readNextMsg(self))  #echo      
+                replies.append (self.readNextMsg(self)) 
+            elif "" in cmd:
+                replies.append (self.readNextMsg(self))  #echo         
+                replies.append (self.readNextMsg(self))
+                replies.append (self.readNextMsg(self)) 
+            else:
+                replies.append (self.readNextMsg(self))  #echo         
+                replies.append (self.readNextMsg(self))
+                replies.append (self.readNextMsg(self))
+                replies.append (self.readNextMsg(self))
+
+
     
-#This line was added in eclipse
+        except Exception as e:
+            print(e)
+            
+        finally:   
+            self.ser.close()        
+            return replies
+
+    
+    
+        
+    #This line was added in eclipse
