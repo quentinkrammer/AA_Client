1c161d77ef661dc5867b6b62961d059dfa2b7a9c
Bastian <bopatzable@gmail.com>
2019-10-30 10:54:07 +0100

    added .gitignore

---
+++ src/gui.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import wx
+
+# Next, create an application object.
+app = wx.App()
+
+# Then a frame.
+frm = wx.Frame(None, title="Hello World")
+
+# Show it.
+frm.Show()
+
+# Start the event loop.
+app.MainLoop()
