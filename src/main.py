f2fe725bfe20bd2818e86580166562a6baa2b5eb
Bastian <bopatzable@gmail.com>
2019-10-30 11:10:15 +0100

    renamedFiles

--- src/main.py
+++ src/main.py
@@ -1,42 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 24 13:12:44 2019
-@author: ip15oheg
-"""
-import serial
- 
-def readNextMsg():
-    msg = ""
-    endOfMsg = False
-    while(not endOfMsg):
-        char = ser.read().decode()        
-        if char == "\n":
-            endOfMsg = True
-        else:
-            msg += char  
-    return msg
 
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.port = 'COM16'
-ser.timeout = 0.500
-ser.open()
+import wx
 
-try:
-    cmd = "SYS:OFF"
-    cmd += "\n"
-    ser.write(cmd.encode())
-    echo = readNextMsg()
-    print(echo)
-    reply = readNextMsg()
-    print(reply)
-    #print(readNextMsg())
-    #print(readNextMsg())
+#Create an application object.
+app = wx.App()
 
-except Exception as e:
-    print(e)
-    
-finally:   
-    ser.close()
-    
-#This line was added in eclipse
+# Then a frame.
+frm = wx.Frame(None, title="Hello World")
+
+# Show it.
+frm.Show()
+
+# Start the event loop.
+app.MainLoop()
---
+++ src/serialCommunication.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 13:12:44 2019
+@author: ip15oheg
+"""
+import serial
+ 
+def readNextMsg():
+    msg = ""
+    endOfMsg = False
+    while(not endOfMsg):
+        char = ser.read().decode()        
+        if char == "\n":
+            endOfMsg = True
+        else:
+            msg += char  
+    return msg
+
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = 'COM16'
+ser.timeout = 0.500
+ser.open()
+
+try:
+    cmd = "SYS:OFF"
+    cmd += "\n"
+    ser.write(cmd.encode())
+    echo = readNextMsg()
+    print(echo)
+    reply = readNextMsg()
+    print(reply)
+    #print(readNextMsg())
+    #print(readNextMsg())
+
+except Exception as e:
+    print(e)
+    
+finally:   
+    ser.close()
+    
+#This line was added in eclipse
